da53675fa953b07d7bdbba272c8b2acb950cb9a1
Sandeep Raju <sandeep@plivo.com>
2014-09-27 00:17:22 +0000

    pep8 formatting.

--- enqueue_sms_1_one.py
+++ enqueue_sms_1_one.py
@@ -2,11 +2,13 @@ import json
 import uuid
 import requests
 
-requests.post('http://localhost:8080/enqueue/sms/1/',
-              data=json.dumps({
-                  # generate unique job_id for each request
-                  'job_id': str(uuid.uuid4()),
-                  'interval': 1000,  # means a rate of 1 job per second
-                  'payload': {'message': 'hello, world - from queue 1'}
-              }),
-              headers={'Content-Type': 'application/json'})
+requests.post(
+    'http://localhost:8080/enqueue/sms/1/',
+    data=json.dumps({
+        # generate unique job_id for each request
+        'job_id': str(uuid.uuid4()),
+        'interval': 1000,  # means a rate of 1 job per second
+        'payload': {'message': 'hello, world - from queue 1'}
+    }),
+    headers={'Content-Type': 'application/json'}
+)
--- interval_sms_1_5000.py
+++ interval_sms_1_5000.py
@@ -1,6 +1,8 @@
 import json
 import requests
 
-response = requests.post('http://localhost:8080/interval/sms/1/',
-                         data=json.dumps({'interval': 5000}),
-                         headers={'Content-Type': 'application/json'})
+response = requests.post(
+    'http://localhost:8080/interval/sms/1/',
+    data=json.dumps({'interval': 5000}),
+    headers={'Content-Type': 'application/json'}
+)
